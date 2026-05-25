f1ba1b6b25d22904f9437188c14194d0034a7bed
Waseem0163 <waseem.rodnani6111862@gmail.com>
2026-05-25 09:30:07 +0500

    designed the page

---
+++ script.js
@@ -0,0 +1,9 @@
+const AmountInputEl = document.getElementById("bill-input-el")
+const tipInputEl = document.getElementById("tip-input-el")
+const totalEl = document.getElementById("total-el")
+
+function calculateTip(){
+    
+}
+
+document.getElementById("calculate-btn").addEventListener("click", calculateTip)
