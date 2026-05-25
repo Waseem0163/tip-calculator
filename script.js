4fa0ca99a245f5477673a9f27e42fecfa42e2182
Waseem0163 <waseem.rodnani6111862@gmail.com>
2026-05-25 09:46:29 +0500

    added JS logic

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const AmountInputEl = document.getElementById("bill-input-el")
 const tipInputEl = document.getElementById("tip-input-el")
 const totalEl = document.getElementById("total-el")
 
+// Calculating the Tip Amount
 function calculateTip(){
-    
+    let billAmount = Number(AmountInputEl.value)
+    let tipAmount = Number(tipInputEl.value)
+    let result = ((billAmount/100) * tipAmount) + billAmount
+
+    totalEl.innerHTML = `Total: <b>PKR${result.toFixed(2)} </b>`
 }
 
 document.getElementById("calculate-btn").addEventListener("click", calculateTip)
